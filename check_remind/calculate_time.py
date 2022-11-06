11d0d7282ba9a334f88ac35f0afb01f78f18e1ad
tkdoon <64641552+tkdoon@users.noreply.github.com>
2022-11-06 15:37:51 +0900

    date+time

--- check_remind/calculate_time.py
+++ check_remind/calculate_time.py
@@ -13,11 +13,11 @@ def calculate_time(dates_list, times_list, messages):
     for index, dates in enumerate(dates_list):
         for date in dates:
             for time in times_list[index]:
-                date_plus_time = date+time
+                date_plus_time = date+"-"+time
                 date_plus_time_of_this_year = str(
-                    datetime.datetime.now(tz).year)+date_plus_time
+                    datetime.datetime.now(tz).year)+"/"+date_plus_time
                 datetime_date_plus_time = tz.localize(datetime.datetime.strptime(
-                    date_plus_time_of_this_year, '%Y%m/%d%H:%M'))
+                    date_plus_time_of_this_year, '%Y/%m/%d-%H:%M'))
                 if datetime_date_plus_time < now:
                     datetime_date_plus_time += relativedelta.relativedelta(
                         years=1)
